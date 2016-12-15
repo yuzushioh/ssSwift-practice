a2507c93b1a8b6ee54fff35ac55f3ea326348e8b
= <yuzushioh@gmail.com>
2016-12-15 15:41:59 +0900

    queryでユーザー名から既に使用されているかを確認する

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,12 +1,31 @@
 import Vapor
-import VaporPostgresSQL
+import VaporPostgreSQL
 
 let drop = Droplet()
 
 drop.preparations.append(Task.self)
-try drop.addProvider(Provider.self)
+drop.preparations.append(User.self)
+try drop.addProvider(VaporPostgreSQL.Provider.self)
 
 drop.resource("users", UserController())
 drop.resource("tasks", TaskController())
+
+drop.get("version") { request in
+    guard let db = drop.database?.driver as? PostgreSQLDriver else {
+        throw Abort.badRequest
+    }
+    
+    let version = try db.raw("SELECT version()")
+    return try JSON(node: version)
+}
+
+drop.get("validation/username") { request in
+    guard let username = request.data["username"]?.string else {
+        throw Abort.badRequest
+    }
+    
+    let users = try User.query().filter("username", username).run()
+    return try JSON(node: ["is_valid": "\(users.isEmpty)"])
+}
 
 drop.run()
